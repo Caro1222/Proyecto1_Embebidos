3c757d1c38d79527a831dd06512e10c3da63458b
Carolina Zuniga Blanco <carolinazb200@gmail.com>
2025-04-16 20:28:39 -0600

    cambios human.py

--- Proyecto_he/code/human.py
+++ Proyecto_he/code/human.py
@@ -6,8 +6,8 @@ from gst_utils import gst_launch
 DEVICE="AUTO"
 
 # paths
-MODELS_PATH="/home/carolina/Proyecto1_Embebidos/Proyecto_he"
-MODELS_PROC_PATH="/home/carolina/Proyecto1_Embebidos/Proyecto_he"
+MODELS_PATH="/home/carolina/Proyecto1_Embebidos/Proyecto_he/models"
+MODELS_PROC_PATH="/home/carolina/Proyecto1_Embebidos/Proyecto_he/model_proc"
 
 # Models
 MODEL_1="human-pose-estimation-0001"
@@ -19,7 +19,7 @@ HPE_MODEL_PROC=f"{MODELS_PROC_PATH}/{MODEL_1}.json"
 HPE_MODEL=f"{MODELS_PATH}/{MODEL_1}.xml"
 
 # Input
-INPUT="{MODELS_PATH}/face-demographics-walking.mp4"
+INPUT="/home/carolina/Proyecto1_Embebidos/Proyecto_he/inputs/face-demographics-walking.mp4"
 
 pipeline_str = (
     f'urisourcebin buffer-size=4096 uri={INPUT} ! '
